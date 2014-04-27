0431ded95e9fe1ab768952884c1578685ecacf8c
Josh Aronoff <aronoff@gmail.com>
2014-04-26 22:43:50 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Podapp::Application.config.secret_key_base = 'b6e25eb1296b7fd52f8106a4a87c8a8be86314d943f13dd3cc01858b83dc27e5d218cb19d7953e4a2e3294cf3edce08eedccfed75bd16ee53f6a8602db2f204d'
